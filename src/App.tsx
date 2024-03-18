48b6349a0f484512414dc606ad3e97716c5d1160
Amumug <fabriali4@gmail.com>
2024-03-18 11:55:53 +0000

    movie-hub

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import './App.css'
+import { Button } from './components/ui/button'
+import { toast } from "sonner"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+
+function App() {
+
+  return (
+    <>
+       <div className="bg-slate-950 p-4">
+      <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind!</h1>
+      <p className="text-red-600 mt-2">Welcome to my Tailwind CSS powered React app.</p>
+      <Button
+      variant="outline"
+      onClick={() =>
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        })
+      }
+    >
+      Show Toast
+    </Button>
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+
+    </div>
+    </>
+  )
+}
+
+export default App
