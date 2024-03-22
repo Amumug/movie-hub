dfd3a7a86c57bfdf9bef37cd6d35b24edc27639d
Amumug <fabriali4@gmail.com>
2024-03-22 06:53:32 +0000

    Movie-hub

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import NavBar from "./components/NavBar";
-
+import { ThemeProvider } from "./components/ThemeProvider"
 
 function App() {
   return <>
     <NavBar></NavBar>
+    <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
   </>;
 }
 
