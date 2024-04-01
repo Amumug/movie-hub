b44b78f29b29194744274cc95339c27f332aa435
Amumug <fabriali4@gmail.com>
2024-04-01 17:25:13 +0000

    movie-hub

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { ModeToggle } from "./components/ModeToggle";
 import NavBar from "./components/NavBar";
-import { ThemeProvider } from "./components/ThemeProvider";
-import { Input } from "./components/ui/input";
 
 function App() {
   return (
