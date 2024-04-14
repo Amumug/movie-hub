fb1b88ed3e1708ceb848bdfd97e61c82422c9f6b
Amumug <fabriali4@gmail.com>
2024-04-14 23:10:03 +0100

    movie-app

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
       <div className="flex justify-between ">
         <div className="flex items-center space-x-2">
           <img src={logo} alt="" />
-          <p className="">AMovie</p>
+          <p className="">AMovies</p>
           <Input
             type="text"
             placeholder="Search for Movies..."
