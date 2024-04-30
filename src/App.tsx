38b61b7190cd95a51f25bf20ad1e392c295f125d
Amumug <fabriali4@gmail.com>
2024-04-30 11:02:37 +0100

    movie-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    const root = window.document.documentElement;
-    isDarkMode ? root.classList.add('dark') : root.classList.remove('dark');
-  }, [isDarkMode]);
-
   return (
-    <div>
-      <button onClick={() => setIsDarkMode(!isDarkMode)}>
-        Toggle Dark Mode
-      </button>
-      {/* Your app content */}
-    </div>
+   <>
+   <NavBar/>
+   </>
   );
 }
 
