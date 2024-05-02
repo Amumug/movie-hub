26f8b5529af20835da8437b0b7d5efa13a8b1bee
Amumug <fabriali4@gmail.com>
2024-05-02 10:51:17 +0100

    movie-app

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,17 @@
 import { ModeToggle } from "./ModeToggle";
 import { ThemeProvider } from "./ThemeProvider";
 import logo from "../assets/Logo-Movie.svg";
+import styles from "./style"
 
 const NavBar = () => {
   return (
     <>
-      <div>
-        <img src={logo} alt="" />
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <ModeToggle />
-        </ThemeProvider>
+      <div className="bg-stone-500 w-full overflow-hidden">
+       <div className="sm:px-16 px-6 flex justify-center items-center">
+        <div className="xl:max-w-[1280px] w-full">
+          Navbar
+        </div>
+       </div>
       </div>
     </>
   );
